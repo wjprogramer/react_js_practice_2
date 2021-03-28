8ad3206409d34efe6664bafd388584078a574594
jay_wu <jay_wu>
2021-03-28 09:33:58 +0800

    -

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
 import styled from '@emotion/styled';
 
-const H1 = styled.h1``
+const H1 = styled.h1``;
+const H2 = styled.h2``;
+const H3 = styled.h3``;
+const H4 = styled.h4``;
+const H5 = styled.h5``;
+const H6 = styled.h6``;
 
 export {
-  H1
+  H1, H2, H3, H4, H5, H6,
 }
