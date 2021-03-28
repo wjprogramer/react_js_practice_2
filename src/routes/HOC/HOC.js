cffd3468efb062ae90ad786e657c8af7de7a30cb
jay_wu <jay_wu>
2021-03-28 09:44:44 +0800

    -

--- src/routes/HOC/HOC.js
+++ src/routes/HOC/HOC.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { H1, H2, H3 } from '../../components/';
 
-import demo1 from './components/demo1';
+import simple from './components/simple';
 import receiveParams from './components/receiveParams';
 
 const HOC = () => {
@@ -13,7 +13,7 @@ const HOC = () => {
       <a href="https://hsien-w-wei.medium.com/react-higher-order-component-%E9%AB%98%E9%9A%8E%E7%B5%84%E4%BB%B6-4110c03043ba" target="_blank" rel="noreferrer">參考連結</a>
 
       <H2>#1 Simple HOC Demo</H2>
-      <demo1.Simple />
+      <simple.Simple />
 
       <H2>#2 HOC 的各種用法</H2>
       <H3>傳入參數</H3>
