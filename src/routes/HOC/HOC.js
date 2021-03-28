fb247545eb5a2a0f082f4881195b6d785b48b8d4
jay_wu <jay_wu>
2021-03-28 09:39:31 +0800

    ✨ [HOC] pass params

--- src/routes/HOC/HOC.js
+++ src/routes/HOC/HOC.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import { H1, H2 } from '../../components/';
+import { H1, H2, H3 } from '../../components/';
 
 import demo1 from './components/demo1';
+import receiveParams from './components/receiveParams';
 
 const HOC = () => {
   return (
@@ -12,9 +13,11 @@ const HOC = () => {
       <a href="https://hsien-w-wei.medium.com/react-higher-order-component-%E9%AB%98%E9%9A%8E%E7%B5%84%E4%BB%B6-4110c03043ba" target="_blank" rel="noreferrer">參考連結</a>
 
       <H2>#1 Simple HOC Demo</H2>
-
       <demo1.Simple />
 
+      <H2>#2 HOC 的各種用法</H2>
+      <H3>傳入參數</H3>
+      <receiveParams.Male />
     </>
   );
 }
---
+++ src/routes/HOC/components/receiveParams.js
@@ -0,0 +1,22 @@
+// 可傳入性別參數的 HOC
+const UserGenderHOC = (gender) => (WrappedComponent) => {
+  const UserGenderHOC = (props) => {
+    return (
+      <WrappedComponent gender={gender} {...props} />
+    );
+  }
+  return UserGenderHOC;
+};
+
+// 要傳入的元件
+const BaseComp = (props) => {
+  const {gender} = props;
+  return (<div>Gender: {gender}</div>); // Gender: Male
+}
+
+// 傳入參數並使用元件
+const Male =  UserGenderHOC('Male')(BaseComp);
+
+
+const result = { Male };
+export default result;
